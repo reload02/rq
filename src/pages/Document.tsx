6498456cc7ddb64b60182f20d69858d289f3e97c
hokyung02 <re-road@naver.com>
2024-12-01 04:33:24 +0900

    feat : 설명 엘리베이터 제작

--- src/pages/Document.tsx
+++ src/pages/Document.tsx
@@ -6,10 +6,10 @@ import Header from "../component/Header";
 const Doc: React.FC = () => {
   const nav = useNavigate();
   return (
-    <div>
+    <div className="flex h-screen flex-col">
       <Header />
-      <div className="flex h-screen">
-        <div className="h-full w-[230px] bg-[#f9f9f9]">
+      <div className="flex flex-1 flex-row overflow-y-auto">
+        <div className="w-[230px] bg-[#f9f9f9]">
           <button
             onClick={() => {
               nav("/");
@@ -23,9 +23,9 @@ const Doc: React.FC = () => {
           <DocsBox boxName="3번 내용들" />
           <DocsBox boxName="4번 내용들" />
         </div>
-        <div className="flex-1 overflow-y-auto">
+        <div className="flex flex-1 justify-center overflow-y-auto">
           <ShowDocs />
-        </div>
+        </div>{" "}
       </div>
     </div>
   );
