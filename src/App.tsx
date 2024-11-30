49a08c05dfab5638754d93b9ec93c9eb0c62c797
Kim Hokyung <re-road@naver.com>
2024-11-30 16:41:41 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./index.css";
+
+function App() {
+  return (
+    <button className="mt-4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
+      테스트
+    </button>
+  );
+}
+
+export default App;
